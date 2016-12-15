3b4ab2cbf74e63232a6de4266abbae7b1824be5c
carcheky <carcheky@gmail.com>
2016-12-15 18:03:36 +0100

    Initial commit

---
+++ app/src/main/java/es/cckdev/carcheky/envoltorio/MainActivity.java
@@ -0,0 +1,21 @@
+package es.cckdev.carcheky.envoltorio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.WebView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("file:///assets/hypeweb/index.html");
+
+    }
+}
