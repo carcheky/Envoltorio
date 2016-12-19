0f6c28ec75b3b6e71cf0692c872af00bf07ed605
carcheky <carcheky@gmail.com>
2016-12-19 17:55:01 +0100

    updated

--- app/src/main/java/es/cckdev/carcheky/envoltorio/MainActivity.java
+++ app/src/main/java/es/cckdev/carcheky/envoltorio/MainActivity.java
@@ -2,6 +2,7 @@ package es.cckdev.carcheky.envoltorio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -11,11 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);
         WebView myWebView = (WebView) findViewById(R.id.WebView);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        myWebView.loadUrl("file:///assets/hypeweb/index.html");
-
+//        myWebView.loadUrl("file:///android_asset/web/index.html");
+        myWebView.loadUrl("http://happy2friend.com");
+        myWebView.getSettings().setLoadWithOverviewMode(true);
+        myWebView.getSettings().setUseWideViewPort(true);
     }
 }
